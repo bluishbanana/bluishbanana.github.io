56ca9edab26c097358e2a70b292623cd594a30c9
user <>
2020-09-13 20:03:20 +0200

    ferffre

--- _site/script.js
+++ _site/script.js
@@ -13,9 +13,14 @@ function debounce(func, wait = 20, immediate = true) {
   };
 };
 
+function* offsetParents(offsetParent) {
+  while (offsetParent !== document.body) {
+    yield offsetParent
+    offsetParent = offsetParent.offsetParent
+  }
+}
 
-
-
+const distanceToBodyTop = (offsetParent) => ([...offsetParents(offsetParent)].reduce((a, v) => a + v.offsetTop, 0))
 
 const sliderImages = document.querySelectorAll('.slide_in');
 
@@ -26,9 +31,8 @@ function checkSlide() {
     // half way through the image
     const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
     // bottom of the image
-
-    const imageBottom = sliderImage.offsetTop + sliderImage.height;
-    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const imageBottom = distanceToBodyTop(sliderImage) + sliderImage.height;
+    const isHalfShown = slideInAt > distanceToBodyTop(sliderImage);
     const isNotScrolledPast = window.scrollY < imageBottom;
 
 
